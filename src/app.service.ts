669a68a12800ecead9a491b1655472973923198e
Udoh Victory <victrudoh@gmail.com>
2025-02-13 12:05:47 +0100

    UPDATE: Added crawl functions on application bootstrap

--- src/app.service.ts
+++ src/app.service.ts
@@ -13,16 +13,16 @@ export class AppService implements OnApplicationBootstrap {
   ) {}
 
   onApplicationBootstrap() {
-    this.scraperService.startAllCompanyScrapers(); // disabled on dev
-    this.productService.getRandomFeaturedItemsByTag(); // disabled on dev
-    this.productService.getRandomFeaturedCategory(); // disabled on dev
+    // this.scraperService.startAllCompanyScrapers(); // disabled on dev
+    // this.productService.getRandomFeaturedItemsByTag(); // disabled on dev
+    // this.productService.getRandomFeaturedCategory(); // disabled on dev
   }
 
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT, {
     name: 'scraperJob', // Give the job a name so it can be managed programmatically
   })
   async handleCron() {
-    this.logger.debug('Called once to add scrape job to queue.');
+    this.logger.debug('Called once to add scrape job to queue..');
     await this.scraperService.startAllCompanyScrapers();
     await this.productService.getRandomFeaturedItemsByTag();
     await this.productService.getRandomFeaturedCategory(); // disabled on dev
