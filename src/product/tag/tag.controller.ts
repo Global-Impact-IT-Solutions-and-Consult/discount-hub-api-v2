83201f84ae9d3ed9570812e5fa87b8e82dbf48e6
kastroud <johnsonolaolu@gmail.com>
2025-06-15 15:39:03 +0100

    feat: implement tag management

--- src/product/tag/tag.controller.ts
+++ src/product/tag/tag.controller.ts
@@ -1,4 +1,66 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { TagService } from './tag.service';
+import { ApiTags } from '@nestjs/swagger';
 
-@Controller('tag')
-export class TagController {}
+@ApiTags('Product|Tag')
+@Controller('product/tag')
+export class TagController {
+  constructor(private readonly tagService: TagService) {}
+
+  @Post()
+  async create(@Body() tagData: { name: string }) {
+    const data = await this.tagService.create(tagData.name);
+    return {
+      success: true,
+      message: 'Tag created successfully',
+      data,
+    };
+  }
+
+  @Get()
+  async findAll() {
+    const data = await this.tagService.findAll();
+    return {
+      success: true,
+      message: 'Tags fetched successfully',
+      data,
+    };
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const data = await this.tagService.findOne(id);
+    return {
+      success: true,
+      message: 'Tag fetched successfully',
+      data,
+    };
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() tagData: { name: string }) {
+    const data = await this.tagService.update(id, tagData.name);
+    return {
+      success: true,
+      message: 'Tag updated successfully',
+      data,
+    };
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    await this.tagService.remove(id);
+    return {
+      success: true,
+      message: 'Tag deleted successfully',
+    };
+  }
+}
--- src/product/tag/tag.service.ts
+++ src/product/tag/tag.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Tag } from './schema/tag.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class TagService {}
+export class TagService {
+  constructor(@InjectModel(Tag.name) private tagModel: Model<Tag>) {}
+
+  async create(name: string) {
+    return this.tagModel.create({ name });
+  }
+
+  async findAll() {
+    return this.tagModel.find().exec();
+  }
+
+  async findOne(id: string) {
+    return this.tagModel.findById(id).exec();
+  }
+
+  async update(id: string, name: string) {
+    return this.tagModel.findByIdAndUpdate(id, { name }, { new: true }).exec();
+  }
+
+  async remove(id: string) {
+    return this.tagModel.findByIdAndDelete(id).exec();
+  }
+
+  async findOrCreate(name: string) {
+    let tag = await this.tagModel.findOne({ name }).exec();
+    if (!tag) {
+      tag = await this.create(name);
+    }
+    return tag;
+  }
+}
