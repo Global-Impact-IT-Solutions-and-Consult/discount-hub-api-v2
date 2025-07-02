6a26e33b4141883524dd8c4aa2bc353cf9c8454c
kastroud <johnsonolaolu@gmail.com>
2025-07-02 02:41:21 +0100

    fix: update JumiaScraperService to include additional Puppeteer launch arguments

--- src/services/scraper/scrapers/jumia.scraper.ts
+++ src/services/scraper/scrapers/jumia.scraper.ts
@@ -24,6 +24,16 @@ export class JumiaScraperService extends WorkerHost {
     try {
       const browser = await puppeteer.launch({
         headless: true,
+        args: [
+          '--no-sandbox',
+          // '--disable-setuid-sandbox',
+          // '--disable-dev-shm-usage',
+          // '--disable-accelerated-2d-canvas',
+          // '--no-first-run',
+          // '--no-zygote',
+          // '--single-process',
+          // '--disable-gpu',
+        ],
       });
       try {
         const page = await browser.newPage();
