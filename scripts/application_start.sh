9ee38564ffca281bd1e3876f5e7b6e929a278eec
kastroud <johnsonolaolu@gmail.com>
2025-10-12 07:13:46 +0100

    chore: add deployment scripts and configuration for EC2 instance setup

---
+++ scripts/application_start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo su
+
+# Navigate to the application directory
+cd /home/ec2-user/discounts-hub-api
+
+# Start the application with PM2
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+#start our node app in the background
+# npm start > app.out.log 2> app.err.log < /dev/null & 
+sudo npm install
+# Start the application using PM2
+sudo pm2 start pm2.config.js --env production
+
+# Make sure the app restarts after reboot
+# sudo pm2 save
+# sudo pm2 startup
---
+++ scripts/before_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Create application directory if it doesn't exist
+mkdir -p /home/ec2-user/discounts-hub-api
