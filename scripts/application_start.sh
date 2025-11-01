800bc0b6531333b6d3ef04b7b5da60313d981bd3
kastroud <johnsonolaolu@gmail.com>
2025-11-01 13:38:06 +0100

    fix: fixed pm2 config

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -12,7 +12,7 @@ export NVM_DIR="$HOME/.nvm"
 # npm start > app.out.log 2> app.err.log < /dev/null & 
 sudo npm install
 # Start the application using PM2
-sudo pm2 start pm2.config.js --env production
+sudo pm2 start ecosystem.config.js --env production
 
 # Make sure the app restarts after reboot
 # sudo pm2 save
